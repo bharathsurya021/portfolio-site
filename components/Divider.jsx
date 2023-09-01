da5167c88366e640b605dffa2672d622b139b9ef
bharathsurya021 <bharathsurya021@gmail.com>
2023-09-01 13:00:31 +0530

    enable divider

--- components/Divider.jsx
+++ components/Divider.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function Divider() {
-  return (
-    <div className="bg-gray-200 my-24 rounded-full h-12 w-1 hidden sm:block"></div>
-  );
+  return <div className="bg-gray-200 my-24 rounded-full h-12 w-1 block"></div>;
 }
